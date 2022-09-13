5b30fda0d7fbd307f5689a896fd2928821939253
DiogoBotton <diogo.luo@gmail.com>
2022-09-13 13:46:19 -0300

    variaveis de ambiente

--- reactjs-revisao/src/pages/Home/Home.js
+++ reactjs-revisao/src/pages/Home/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-export default function Home(){
-    return(
-        <h1>Home</h1>
+export default function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>Ambiente: <b>{process.env.NODE_ENV}</b></p>
+            <p>URL API: <b>{process.env.REACT_APP_LINK_API}</b></p>
+        </div>
     );
 }
